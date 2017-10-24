6b037f1d1e754716533ce5f65ff272af928faab7
katherinebui <kathy.codes@gmail.com>
2017-10-24 09:23:07 -0700

    add key to prop

--- js/CampFeatureListItem.jsx
+++ js/CampFeatureListItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const CampFeatureListItem = props => <div className="cf-list-item">{props.feature.title}</div>;
+const CampFeatureListItem = props => <div className="cf-list-item">{props.title}</div>;
 
 export default CampFeatureListItem;
--- js/CampFeaturesList.jsx
+++ js/CampFeaturesList.jsx
@@ -3,7 +3,7 @@ import FEATURES from '../camp_features';
 import CampFeatureListItem from './CampFeatureListItem';
 
 const CampFeaturelist = () => (
-  <div className="cf-list">{FEATURES.map(feature => <CampFeatureListItem feature={feature} />)}</div>
+  <div className="cf-list">{FEATURES.map(feature => <CampFeatureListItem key={feature.title} {...feature} />)}</div>
 );
 
 export default CampFeaturelist;
