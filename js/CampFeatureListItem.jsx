77d8822c88e96b354fb4b450205735aa17cac025
katherinebui <kathy.codes@gmail.com>
2017-10-24 09:10:26 -0700

    extract list item to different component

---
+++ js/CampFeatureListItem.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CampFeatureListItem = props => <div className="cf-list-item">{props.feature.title}</div>;
+
+export default CampFeatureListItem;
--- js/CampFeaturesList.jsx
+++ js/CampFeaturesList.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import FEATURES from '../camp_features';
+import CampFeatureListItem from './CampFeatureListItem';
 
-const CampFeaturelist = () => <div className="cf-list">{FEATURES.map(feature => <h3>{feature.title}</h3>)}</div>;
+const CampFeaturelist = () => (
+  <div className="cf-list">{FEATURES.map(feature => <CampFeatureListItem feature={feature} />)}</div>
+);
 
 export default CampFeaturelist;
