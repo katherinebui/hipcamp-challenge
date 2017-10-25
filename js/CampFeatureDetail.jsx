088e6aa78e07f5beee5b20e942a2164673b80633
katherinebui <kathy.codes@gmail.com>
2017-10-24 17:27:16 -0700

    stop for today

--- js/CampFeatureDetail.jsx
+++ js/CampFeatureDetail.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CampFeatureDetail = (props: { feature: Feature }) => {
   const { title, presence, subfeatures } = props.feature;
+
+  const renderSubfeatures = () => {
+    if (subfeatures) {
+      subfeatures.map(sf => {
+        console.log(sf);
+      });
+    }
+  };
+
   return (
     <div className="details">
       <header>
         <h1>{title}</h1>
+        {renderSubfeatures()}
       </header>
     </div>
   );
